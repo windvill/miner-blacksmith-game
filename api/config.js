bfdd8682a4aaeb6909e92016627b87be761b387e
windvill <gs112951@gmail.com>
2026-07-03 22:43:35 +0900

    Delete api directory

--- api/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = function handler(_req, res) {
-  res.setHeader("Cache-Control", "no-store");
-  res.status(200).json({
-    supabaseUrl: process.env.SUPABASE_URL || "",
-    supabaseAnonKey: process.env.SUPABASE_ANON_KEY || ""
-  });
-};
