f101490fb1011725b10dd1ff6908ce5a023c7511
windvill <gs112951@gmail.com>
2026-07-03 22:44:38 +0900

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,7 @@
+module.exports = function handler(_req, res) {
+  res.setHeader("Cache-Control", "no-store");
+  res.status(200).json({
+    supabaseUrl: process.env.SUPABASE_URL || "",
+    supabaseAnonKey: process.env.SUPABASE_ANON_KEY || ""
+  });
+};
